350147506dfcb9b7d74b95b18c3408cd99ba5f19
Wahaj-Arsal <83618649+Wahaj-Arsal@users.noreply.github.com>
2022-12-15 01:23:25 +0000

    mock data added, header done, manage teams tab done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import Topbar from "./pages/global/Topbar.jsx";
+import Dashboard from "./pages/dashboard/Dashboard.jsx";
+import Sidebar from "./pages/global/Sidebar.jsx";
+import Team from "./pages/team/Team.jsx";
+import Contacts from "./pages/contacts/Contacts.jsx";
+
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
-
 export default App;
